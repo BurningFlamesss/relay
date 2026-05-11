f72fa13931e79329b49e33bdbc8953944db808a4
AD <burningggflamesss@gmail.com>
2026-05-11 22:35:13 +0545

    add tanstack query provider

--- src/provider/GlobalProvider.tsx
+++ src/provider/GlobalProvider.tsx
@@ -1,14 +1,19 @@
 import { Toaster } from '#/components/ui/sonner.tsx';
 import { TooltipProvider } from '#/components/ui/tooltip'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react'
+
+const queryClient = new QueryClient()
 
 function GlobalProvider({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <TooltipProvider>
-                {children}
-                <Toaster />
-            </TooltipProvider>
+            <QueryClientProvider client={queryClient}>
+                <TooltipProvider>
+                    {children}
+                    <Toaster />
+                </TooltipProvider>
+            </QueryClientProvider>
         </>
     )
 }
