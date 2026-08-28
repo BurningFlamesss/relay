43aa565377eccbbcb004d48982d4f065ba767908
AD <burningggflamesss@gmail.com>
2026-08-28 21:49:49 +0545

    feat(queues): integrate QUEUE constants and enhance makeQueue function with type options

--- src/core/queues.ts
+++ src/core/queues.ts
@@ -1,7 +1,9 @@
+import { preprocess } from 'better-auth';
 import { Queue, type JobsOptions } from "bullmq";
 import { connection } from "./connection";
 import type { Stage } from "#/hooks/useAnalysis.tsx";
 import { getUtilityConnection } from "./redis";
+import { QUEUE } from "./types";
 
 export type AnalyzeJobData = {
     jobId: string;
@@ -43,9 +45,38 @@ const CRITICAL: JobsOptions = {
     priority: 1
 }
 
-function makeQueue(name, options) {
+function makeQueue(name, options?: JobsOptions) {
     return new Queue(name, {
         connection: getUtilityConnection(),
         defaultJobOptions: options ?? BASE
     })
 }
+
+// Queues
+
+export const orchestratorQueue = makeQueue(QUEUE.ORCHESTRATOR, CRITICAL)
+export const scraperQueue = makeQueue(QUEUE.SCRAPER, {
+    ...BASE,
+    attempts: 4,
+    backoff: { type: "exponential", delay: 1000 }
+})
+export const preprocessQueue = makeQueue(QUEUE.PREPROCESS, {
+    ...BASE,
+    backoff: { type: "fixed", delay: 1000 }
+})
+export const aiQueue = makeQueue(QUEUE.AI, {
+    ...CRITICAL,
+    attempts: 4,
+    backoff: { type: "exponential", delay: 5000 }
+})
+export const scoringQueue = makeQueue(QUEUE.SCORING, {
+    ...BASE,
+    backoff: { type: "fixed", delay: 500 }
+})
+export const dlQueue = makeQueue(QUEUE.DLQ, {
+    removeOnComplete: false,
+    removeOnFail: false,
+    attempts: 1
+})
+
+
